a8813bd980aeaf6488def3f6402b7ef4c7cfb62c
khan9920 <shafkhanmohamed23@gmail.com>
2020-11-25 19:05:33 +0530

    header completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { HeaderComponent } from './landingpage/header/header.component';
+import { ExamplesComponent } from './landingpage/examples/examples.component';
+import { TextEditorComponent } from './landingpage/text-editor/text-editor.component';
+import { TextProcessedComponent } from './landingpage/text-processed/text-processed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    HeaderComponent,
+    ExamplesComponent,
+    TextEditorComponent,
+    TextProcessedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/landingpage/text-processed/text-processed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-processed',
+  templateUrl: './text-processed.component.html',
+  styleUrls: ['./text-processed.component.css']
+})
+export class TextProcessedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
