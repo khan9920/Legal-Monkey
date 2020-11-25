f0342969aa10918d6e7f0f5aff773c0b1449d14a
khan9920 <shafkhanmohamed23@gmail.com>
2020-11-25 21:36:23 +0530

    Swiper JS added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { HeaderComponent } from './landingpage/header/header.component';
 import { ExamplesComponent } from './landingpage/examples/examples.component';
 import { TextEditorComponent } from './landingpage/text-editor/text-editor.component';
 import { TextProcessedComponent } from './landingpage/text-processed/text-processed.component';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
 
 @NgModule({
   declarations: [
@@ -20,9 +30,13 @@ import { TextProcessedComponent } from './landingpage/text-processed/text-proces
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
-  providers: [],
+  providers: [{
+    provide: SWIPER_CONFIG,
+    useValue: DEFAULT_SWIPER_CONFIG
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/landingpage/examples/examples.component.ts
+++ src/app/landingpage/examples/examples.component.ts
@@ -12,4 +12,26 @@ export class ExamplesComponent implements OnInit {
   ngOnInit() {
   }
 
+  slides = [
+    {
+      input: ' Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
+      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+    },
+    {
+      input: ' Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
+      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+    },
+    {
+      input: '  3 Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
+      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+    },
+    {
+      input: ' 3 Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
+      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+    },
+    {
+      input: ' Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
+      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+    },
+  ];
 }
