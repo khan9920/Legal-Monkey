da70ad25cf0b9cfece3f531651a8a144af9a5067
khan9920 <shafkhanmohamed23@gmail.com>
2020-12-08 13:40:17 +0530

    Heero text adjusted

--- src/app/components/landingpage/hero/hero.component.ts
+++ src/app/components/landingpage/hero/hero.component.ts
@@ -11,24 +11,35 @@ export class HeroComponent implements OnInit {
 
   slides = [
     {
-      input: 'Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”).',
-      output: 'If you want to sell your shares, you must first offer them to the other shareholders.'
+      input: 'Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”).',
+      outputs: [
+        'If you buy, get, license, rent or subscribe to content, Apps and other in-app services in the Apple Online Store (or a third party online store using the in-app purchase API), this agreement governs your use of Apple’s services.',
+        'Content may be offered by Apple or a third party. Our Services are available for your use in your country of residence.'
+      ]
     },
     {
       input: ' Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
-      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+      outputs: ['If you want to sell your shares, you must first offer them to the other shareholders.']
     },
     {
       input: '  3 Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
-      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+      outputs: [
+        'If you want to sell your shares, you must first offer them to the other shareholders.',
+        'If you want to sell your shares, you must first offer them to the other shareholders.',
+        'If you want to sell your shares, you must first offer them to the other shareholders.'
+      ]
     },
     {
       input: ' 3 Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
-      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+      outputs: ['If you want to sell your shares, you must first offer them to the other shareholders.']
     },
     {
       input: ' Each Shareholder(an “Oferor”) proposing to Transfer any oral of the Shares then owned by such Oferor (the “Transfer Shares”) to any third party (the “Oferee”), such Oferor shall first offer Transfer Shares to the other Shareholders (“RemainingShareholders”). ',
-      output: ' If you want to sell your shares, you must first offer them to the other shareholders.'
+      outputs: [
+        'If you want to sell your shares, you must first offer them to the other shareholders.',
+        'If you want to sell your shares, you must first offer them to the other shareholders.',
+        'If you want to sell your shares, you must first offer them to the other shareholders.'
+      ]
     },
   ];
 
@@ -37,13 +48,13 @@ export class HeroComponent implements OnInit {
   public config: SwiperOptions = {
     a11y: { enabled: true },
     slidesPerView: 1,
-    keyboard: true,
+    keyboard: false,
     mousewheel: false,
     scrollbar: false,
-    navigation: false,
+    navigation: true,
     pagination: false,
-    autoplay: true,
-    spaceBetween: 30
+    autoplay: false,
+    spaceBetween: 30,
   };
 
   constructor() { }
