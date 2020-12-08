d66a7fe331ad9a238d868072187b9be70d2eb2c5
khan9920 <shafkhanmohamed23@gmail.com>
2020-12-08 13:52:41 +0530

    Login button added to header section

--- src/app/components/landingpage/header/header.component.ts
+++ src/app/components/landingpage/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public isAuthenticated: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
