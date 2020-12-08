7baf841e68d09ffb40eef41cddccb086eae200cd
khan9920 <shafkhanmohamed23@gmail.com>
2020-12-09 04:51:01 +0530

    Form validations comopeted on sign up form

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { LoginComponent } from '../login/login.component';
+import * as EmailValidator from 'email-validator';
 
 @Component({
   selector: 'app-signup',
@@ -21,13 +22,13 @@ export class SignupComponent implements OnInit, OnDestroy {
     confirmPassword: '',
   }
 
-  public userValidation = {
-    firstName: 'valid',
-    lastName: 'valid',
-    email: 'valid',
-    mobile: 'valid',
-    password: 'valid',
-    confirmPassword: 'valid',
+  public validation = {
+    firstName: true,
+    lastName: true,
+    email: true,
+    mobile: true,
+    password: true,
+    confirmPassword: true
   }
 
   public isLoading: boolean = false;
@@ -42,26 +43,76 @@ export class SignupComponent implements OnInit, OnDestroy {
   }
 
   onSignUp() {
-    if (this.user.firstName == '' || this.user.lastName == '' || this.user.email == '' || this.user.mobile == '' || this.user.password == '' || this.user.confirmPassword == '') {
-      this.snackBar.open('All fields are required.', 'Dismiss', {
-        duration: 3000
-      });
+    this.validator();
+    if (!this.validation.firstName || !this.validation.lastName || !this.validation.email || !this.validation.mobile || !this.validation.password || !this.validation.confirmPassword) {
       return;
-    } else if (this.user.password !== this.user.confirmPassword) {
-      this.snackBar.open('Passwords do not match', 'Dismiss', {
-        duration: 3000
-      });
-      return;
+    }
+
+    const user = {
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      email: this.user.email,
+      mobile: this.user.mobile,
+      password: this.user.password
+    }
+    this.authService.setLoadingStatus(true);
+    this.authService.signUp(user);
+  }
+
+  private validator() {
+    if (this.user.firstName == '') {
+      this.validation.firstName = false;
     } else {
-      const user = {
-        firstName: this.user.firstName,
-        lastName: this.user.lastName,
-        email: this.user.email,
-        mobile: this.user.mobile,
-        password: this.user.password
+      this.validation.firstName = true;
+    }
+
+    if (this.user.lastName == '') {
+      this.validation.lastName = false;
+    } else {
+      this.validation.lastName = true;
+    }
+
+    if (this.user.email == '') {
+      this.validation.email = false;
+    } else {
+      this.validation.email = true;
+    }
+
+    if (this.user.email !== '') {
+      if (!EmailValidator.validate(this.user.email)) {
+        this.validation.email = false;
+        this.snackBar.open('Please enter a valid email', 'Dismiss', {
+          duration: 3000
+        });
       }
-      this.authService.setLoadingStatus(true);
-      this.authService.signUp(user);
+    }
+
+    if (this.user.mobile == '') {
+      this.validation.mobile = false;
+    } else {
+      this.validation.mobile = true;
+    }
+
+    if (this.user.password == '') {
+      this.validation.password = false;
+    } else {
+      this.validation.password = true;
+    }
+
+    if (this.user.confirmPassword == '') {
+      this.validation.confirmPassword = false;
+    } else {
+      this.validation.confirmPassword = true;
+    }
+
+    if (this.user.password !== '' && this.user.confirmPassword !== '') {
+      if (this.user.password !== this.user.confirmPassword) {
+        this.validation.password = false;
+        this.validation.confirmPassword = false;
+        this.snackBar.open('Passwords do not match', 'Dismiss', {
+          duration: 3000
+        });
+      }
     }
   }
 
