5db0431b0bfe3f7272e705fd9e475d9a638d94a4
khan9920 <shafkhanmohamed23@gmail.com>
2021-01-07 16:21:33 +0530

    Old conversions integrated

--- src/app/components/conversions/conversions.component.ts
+++ src/app/components/conversions/conversions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { ConversionsService } from 'src/app/services/conversions.service';
@@ -14,23 +15,47 @@ export class ConversionsComponent implements OnInit {
   public recentClicked: boolean = true;
   public allClicked: boolean = false;
 
-  constructor(private conversionsService: ConversionsService, private authService: AuthService, private router: Router) { }
+  constructor(private conversionsService: ConversionsService, private authService: AuthService, private router: Router, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    this.conversions = this.conversionsService.getRecentConversions();
+    this.conversionsService.getRecentConversions().subscribe(result => {
+      if (result.success) {
+        this.conversions = result.data;
+      }
+    }, error => {
+      this.snackBar.open(error.error.data, 'Dismiss', {
+        duration: 3000
+      });
+    });
   }
 
   onRecent() {
     this.recentClicked = true;
     this.allClicked = false;
-    this.conversions = this.conversionsService.getRecentConversions();
+    this.conversionsService.getRecentConversions().subscribe(result => {
+      if (result.success) {
+        this.conversions = result.data;
+      }
+    }, error => {
+      this.snackBar.open(error.error.data, 'Dismiss', {
+        duration: 3000
+      });
+    });
   }
 
   onAll() {
     this.recentClicked = false;
     this.allClicked = true;
 
-    this.conversions = this.conversionsService.getAllConversions();
+    this.conversionsService.getAllConversions().subscribe(result => {
+      if (result.success) {
+        this.conversions = result.data;
+      }
+    }, error => {
+      this.snackBar.open(error.error.data, 'Dismiss', {
+        duration: 3000
+      });
+    });
   }
 
   onBackToHome() {
