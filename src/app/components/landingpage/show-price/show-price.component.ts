b0c8545b247b5f9ea34efed544ced15379decb42
khan9920 <shafkhanmohamed23@gmail.com>
2021-02-06 22:25:10 +0530

    Erros fixed :

--- src/app/components/landingpage/show-price/show-price.component.ts
+++ src/app/components/landingpage/show-price/show-price.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ConversionsService } from 'src/app/services/conversions.service';
+import { SimplifyService } from 'src/app/services/simplify.service';
 
 @Component({
   selector: 'app-show-price',
@@ -11,18 +12,36 @@ import { ConversionsService } from 'src/app/services/conversions.service';
 export class ShowPriceComponent implements OnInit {
 
   public price: number;
+  public wordCount: number;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private consersionsService: ConversionsService, private dialog: MatDialog, private snackBar: MatSnackBar) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private consersionsService: ConversionsService, private simplifyService: SimplifyService, private dialog: MatDialog, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.consersionsService.calculatePrice(this.data).subscribe(result => {
       if (result.success) {
-        this.price = result.data
+        if (result.data.price === 0) {
+          this.onContinue();
+        }
+        this.price = result.data.price;
+        this.wordCount = result.data.words;
       }
     }, error => {
       this.snackBar.open(error.error.data, 'Dismiss', {
         duration: 3000
       });
+    });
+  }
+
+  onContinue(): void {
+
+    console.log(this.data);
+
+    this.simplifyService.simplify(this.data).subscribe(result => {
+      if (result.success) {
+        this.dialog.closeAll();
+        console.log(result.data);
+
+      }
     });
   }
 
