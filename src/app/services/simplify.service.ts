c9db8e9a6a7b0b919de4af58ce6e111722952613
khan9920 <shafkhanmohamed23@gmail.com>
2021-02-07 18:47:23 +0530

    Simple text conversions restuls are showing now

--- src/app/services/simplify.service.ts
+++ src/app/services/simplify.service.ts
@@ -13,10 +13,20 @@ export class SimplifyService {
   editorStatus = new Subject<any>();
   simplifiedDocumentSub = new Subject<any>();
 
+  simplifiedTextSub = new Subject<any>();
+
   constructor(private http: HttpClient) { }
 
   simplify(data) {
     return this.http.post<{ success: boolean, data: any }>(`${apiURL}/extracts/simplify`, data);
+  }
+
+  setSimplifiedData(data) {
+    this.simplifiedDocumentSub.next(data);
+  }
+
+  getSimplifiedData() {
+    return this.simplifiedTextSub.asObservable();
   }
 
   uploadDocuments(data) {
