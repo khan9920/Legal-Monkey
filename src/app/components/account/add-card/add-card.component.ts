6f51141da94a3d1dba86192f573cab6575662d97
khan9920 <shafkhanmohamed23@gmail.com>
2021-03-10 15:23:21 +0530

    IPG Integrated

--- src/app/components/account/add-card/add-card.component.ts
+++ src/app/components/account/add-card/add-card.component.ts
@@ -1,6 +1,11 @@
+declare var $: any;
+declare var WebxpayTokenizeInit: any;
+import { get } from 'scriptjs';
+
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MeService } from 'src/app/services/me.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-card',
@@ -9,19 +14,112 @@ import { MeService } from 'src/app/services/me.service';
 })
 export class AddCardComponent implements OnInit {
 
-  constructor(private meService: MeService, private dialog: MatDialog) { }
+  constructor(private meService: MeService, private dialog: MatDialog, private snackBar: MatSnackBar) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
 
-  onAddCard() {
-    const data = {
-      session: 'SESSION0002409723047L75717837H8'
-    }
-    this.meService.addCard(data);
-  }
+    // const cardServices = CardService;
+    const scope = this;
+    // webxpay codes
+    // init webxpay
+    get("https://cbcmpgs.gateway.mastercard.com/form/version/52/merchant/WEBXPATOKLKR/session.js", () => {
+      WebxpayTokenizeInit({
+        card: {
+          number: "#card-number",
+          securityCode: "#security-code",
+          expiryMonth: "#expiry-month",
+          expiryYear: "#expiry-year",
+          nameOnCard: "#cardholder-name",
+        },
+        ready: afterInit,
+      });
 
-  onClose() {
-    this.dialog.closeAll();
+      function afterInit(GenerateSession) {
+        // save card
+        $('#save-card-button').click(function () {
+          $('#save-card-button').attr('disabled', true);
+
+          GenerateSession(
+            function (session) {
+              handleSuccess(session);
+            },
+            function (error) {
+              handleErrors(error);
+            }
+          );
+        });
+      }
+
+      // execute when hosted session successfully generated
+      function handleSuccess(sessionId) {
+
+        const data = {
+          session: sessionId
+        }
+
+        scope.meService.addCard(data);
+      }
+      // execute when hosted session generation fail.
+      function handleErrors(error) {
+        $('#save-card-button').removeAttr('disabled');
+        $('#save-card-button').html('Save');
+
+        $('.err').html('');
+        $('.general-error').html('');
+
+        switch (error.type) {
+          case 'fields_in_error': {
+            if (error.details.cardNumber) {
+              if (error.details.cardNumber == 'missing') {
+                $('.card-number-error').html('Enter valid card number');
+              }
+              if (error.details.cardNumber == 'invalid') {
+                $('.card-number-error').html('Invalid card number');
+              }
+            }
+            if (error.details.expiryMonth) {
+              if (error.details.expiryMonth == 'missing') {
+                $('.exp-month-error').html('Enter expiration month');
+              }
+              if (error.details.expiryMonth == 'invalid') {
+                $('.exp-month-error').html('Invalid expiration month');
+              }
+            }
+            if (error.details.expiryYear) {
+              if (error.details.expiryYear == 'missing') {
+                $('.exp-year-error').html('Enter expiration year');
+              }
+              if (error.details.expiryYear == 'invalid') {
+                $('.exp-year-error').html('Invalid expiration year');
+              }
+            }
+            if (error.details.securityCode) {
+              if (error.details.securityCode == 'missing') {
+                $('.cvv-error').html('Enter CVV');
+              }
+              if (error.details.securityCode == 'invalid') {
+                $('.cvv-error').html('Invalid CVV');
+              }
+            }
+            console.error('missing card details', error.details);
+            break;
+          }
+          case 'request_timeout': {
+            $('.general-error').html('<span class="text-decoration-uppercase">' + error.details + '</span>')
+            console.error('request time out', error.details);
+            break;
+          }
+          case 'system_error': {
+            if (error.details == 'cvv missing') {
+              $('.general-error').html('Enter CVV details');
+            } else {
+              $('.general-error').html(error.details);
+            }
+            console.error('system error', error.details);
+            break;
+          }
+        }
+      }
+    });
   }
 }
