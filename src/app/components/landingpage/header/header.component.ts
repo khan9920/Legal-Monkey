d081218ad0dac86c126b8b4a4177962de867a8b4
khan9920 <shafkhanmohamed23@gmail.com>
2021-04-03 07:10:21 +0530

    JWT check issue fixed

--- src/app/components/landingpage/header/header.component.ts
+++ src/app/components/landingpage/header/header.component.ts
@@ -27,19 +27,19 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     const token = localStorage.getItem('token');
 
-    if (token !== '' && token !== null) {
-      this.isAuthenticated = true;
+    if (token == '' || token == null) {
+      this.isAuthenticated = false;
+    } else {
+      this.meService.setLoadingStatus(true);
+      this.meService.getMe();
+      this.meSub = this.meService.getMeUpdated().subscribe(result => {
+        this.me = result;
+      });
+
+      this.isAuthenticatedSub = this.authService.getAuthenticationStatus().subscribe(result => {
+        this.isAuthenticated = result;
+      });
     }
-
-    this.meService.setLoadingStatus(true);
-    this.meService.getMe();
-    this.meSub = this.meService.getMeUpdated().subscribe(result => {
-      this.me = result;
-    });
-
-    this.isAuthenticatedSub = this.authService.getAuthenticationStatus().subscribe(result => {
-      this.isAuthenticated = result;
-    });
   }
 
   onSignup(): void {
