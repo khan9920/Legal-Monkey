bda9d19cd15498ad2aa2620d56b2fa664acbd5eb
khan9920 <shafkhanmohamed23@gmail.com>
2021-04-09 15:25:49 +0530

    GAnalatics connected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -7,9 +11,20 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private authService: AuthService) { }
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.authService.autoAuth();
+
+    const navEndEvents = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
+
+    navEndEvents.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-193482270-1', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
   }
 }
