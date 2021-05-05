f2a50873f5d93e0db4fbd32fa6563480b996b8f1
khan9920 <shafkhanmohamed23@gmail.com>
2021-05-05 20:18:52 +0530

    UI done

--- src/app/components/landingpage/document-conversion/document-conversion.component.ts
+++ src/app/components/landingpage/document-conversion/document-conversion.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-document-conversion',
   templateUrl: './document-conversion.component.html',
-  styleUrls: ['./document-conversion.component.css']
+  styleUrls: ['./document-conversion.component.scss']
 })
 export class DocumentConversionComponent implements OnInit {
 
-  constructor() { }
+  public selected: boolean = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onGoBack() {
+    this.router.navigate(['/']);
+  }
+
+  onSelect() {
+    if (this.selected) {
+      this.selected = false;
+    } else {
+      this.selected = true;
+    }
+
+    console.log(this.selected);
+
+  }
 }
